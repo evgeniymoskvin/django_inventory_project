6fbcf2cf5f2e0099c66d77856286ea373b214a5c
E.I.Moskvin <evgeni.moskvin@gmail.com>
2024-11-11 11:14:39 +0300

    upd db & access

--- get_inventory_app/views.py
+++ get_inventory_app/views.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import os
 import logging
@@ -49,12 +50,57 @@ class IndexView(View):
         type_of_inventory_number = int(request.POST['type_of_inventory_number'])
         count_of_inventory_number = int(request.POST['count_of_inventory_number'])
         final_inventory_list = []
+        today_year_short = int(str(datetime.date.today().year)[-2:])
+
         try:
             cpe = CpeModel.objects.get_queryset().filter(cpe_object_id=object_id).filter(cpe_important=True).first()
-            cpe_str = f'{cpe.cpe_user.last_name} {cpe.cpe_user.first_name[:1]}.{cpe.cpe_user.middle_name[:1]}'
+            cpe_emp = cpe.cpe_user
+            cpe_str = f'{cpe_emp.last_name} {cpe_emp.first_name[:1]}.{cpe_emp.middle_name[:1]}.'
         except Exception as e:
+            cpe_emp = None
             print(e)
             cpe_str = 'Не указан'
+
+        # Получение данных из БД
+        # for i in range(count_of_inventory_number):
+        #     general_info = GeneralInfoInventoryNumberModel()
+        #     general_info.order_id = order_id
+        #     general_info.object_name_id = object_id
+        #     general_info.employee = employee
+        #     general_info.cpe = cpe_emp
+        #     general_info.save()
+        #     if type_of_inventory_number == 1:
+        #         # Открытый инвентарный номер
+        #         new_inventory_number = OpenInventoryNumbersModel()
+        #         last_inventory_number_from_db = OpenInventoryNumbersModel.objects.last()
+        #         last_inventory_number_list = str(last_inventory_number_from_db.inventory_number).split('-')
+        #         if int(last_inventory_number_list[0]) == today_year_short:
+        #             last_inventory_number_str = str(int(last_inventory_number_list[1]) + 1)
+        #             new_inventory_number.inventory_number = f'{today_year_short}-{last_inventory_number_str.rjust(5, "0")}'
+        #         else:
+        #             new_inventory_number.inventory_number = f'{today_year_short}-00001'
+        #         new_inventory_number.general_info = general_info
+        #         new_inventory_number.save()
+        #         logging.info(f'Взят открытый номер {new_inventory_number.inventory_number}. {general_info.date_add}')
+        #     elif type_of_inventory_number == 2:
+        #         # Закрытый инвентарный номер
+        #         new_inventory_number = CloseInventoryNumbersModel()
+        #         last_close_inventory_number = CloseInventoryNumbersModel.objects.last()
+        #         last_close_inventory_number_list = str(last_close_inventory_number.inventory_number).split('/')
+        #         last_close_inventory_number_list_num = int(last_close_inventory_number_list[0])+1
+        #         last_close_inventory_number_list_str = f'{str(last_close_inventory_number_list_num).rjust(5, "0")}'
+        #         new_inventory_number.inventory_number = f'{last_close_inventory_number_list_str}/ДСП'
+        #         new_inventory_number.general_info = general_info
+        #         new_inventory_number.save()
+        #         logging.info(f'Взят закрытый номер {new_inventory_number.inventory_number}. {general_info.date_add}')
+        #     else:
+        #         logging.warning(f'Тип документации {type_of_inventory_number} не найден')
+        #         general_info.delete()
+        #         return HttpResponse(status=500)
+        #     print(new_inventory_number.inventory_number)
+        #     final_inventory_list.append(new_inventory_number.inventory_number)
+
+        # Получение инвентарного из БД access
         for i in range(count_of_inventory_number):
             r = requests.post(f'{API_ADDRESS}/post', params={'order': f'{order_number}.{object_name}',
                                                              'employee': f'{employee.last_name} {employee.first_name[:1]}.{employee.middle_name[:1]}.',
@@ -67,6 +113,7 @@ class IndexView(View):
             general_info = GeneralInfoInventoryNumberModel()
             general_info.order_id = order_id
             general_info.object_name_id = object_id
+            general_info.cpe = cpe_emp
             general_info.employee = employee
             general_info.save()
             if type_of_inventory_number == 1:
@@ -90,6 +137,8 @@ class IndexView(View):
             final_inventory_list.append(new_inventory_number.inventory_number)
         content = {
             'final_inventory_list': final_inventory_list,
+            'count_inventory': len(final_inventory_list),
+            'count_columns': len(final_inventory_list) // 20 + 1,
         }
 
         return render(request, 'get_inventory_app/ajax/get_inventory_done.html', content)
@@ -127,10 +176,8 @@ class DetailsInventoryNumberModalView(View):
             inventory_number_object = CloseInventoryNumbersModel.objects.get(id=object_id)
         else:
             inventory_number_object = None
-        cpe = CpeModel.objects.get_queryset().filter(
-            cpe_object_id=inventory_number_object.general_info.object_name_id).filter(cpe_important=True).first()
         content = {'obj': inventory_number_object,
-                   'cpe': f'{cpe.cpe_user.last_name} {cpe.cpe_user.first_name[:1]}.{cpe.cpe_user.middle_name[:1]}'}
+                   }
         return render(request, 'get_inventory_app/ajax/details_inventory_number.html', content)
 
 
@@ -206,6 +253,7 @@ class GetPermissionView(View):
 
 class MyPermissionNumbers(View):
     """Номера разрешений пользователя"""
+
     @method_decorator(login_required(login_url='login'))
     def get(self, request):
         employee = EmployeeModel.objects.get(user=request.user)
@@ -215,8 +263,10 @@ class MyPermissionNumbers(View):
         content = {'permission_numbers': permission_numbers}
         return render(request, 'get_inventory_app/employee_permission_numbers.html', content)
 
+
 class MyReplacementPermissionNumbers(View):
     """Номера разрешений пользователя"""
+
     @method_decorator(login_required(login_url='login'))
     def get(self, request):
         employee = EmployeeModel.objects.get(user=request.user)
@@ -225,6 +275,7 @@ class MyReplacementPermissionNumbers(View):
             general_info__employee=employee).order_by('-id')
         content = {'replacement_permission_numbers': replacement_permission_numbers}
         return render(request, 'get_inventory_app/employee_replacement_permission_numbers.html', content)
+
 
 class DetailsPermissionNumberModalView(View):
     def get(self, request):
@@ -241,4 +292,3 @@ class DetailsPermissionNumberModalView(View):
         content = {'obj': inventory_number_object,
                    'cpe': f'{cpe.cpe_user.last_name} {cpe.cpe_user.first_name[:1]}.{cpe.cpe_user.middle_name[:1]}'}
         return render(request, 'get_inventory_app/ajax/details_inventory_number.html', content)
-
